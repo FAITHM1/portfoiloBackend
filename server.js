3faac0107c9b734397792238d052e3a723fe276f
faith musa <faithy@faiths-MacBook-Air.local>
2021-11-09 15:01:54 -0600

    backend for my portfolio

---
+++ server.js
@@ -0,0 +1,24 @@
+//dependencies
+const express = require("express");
+const cors = require("cors");
+//importing json file
+const projects = require("./project.json");
+const about = require("./about.json");
+//app
+const app = express();
+//middle ware
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+//project route to get data
+app.get("/projects", (req, res) => {
+  res.json(projects);
+});
+app.get("/about", (req, res) => {
+  res.json(about);
+});
+
+const PORT = 4501;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
